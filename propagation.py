e2988238311fff4f3901fe6c8582040d2c809146
beisNssk <108149093+beisNssk@users.noreply.github.com>
2025-08-20 14:38:49 -0500

    made out error grad

--- propagation.py
+++ propagation.py
@@ -1,27 +1,34 @@
 import math
 import random
+'''
+weighted sum is z
+h is hidden layer
+'''
 
 LEARNING_RATE = 0.01
 BIAS = 1
 
-def sigmoid(x):
+def sig(x):
     return 1 / (1 + math.exp(-x))
 
-def sigmoid_derivative(x):
+def sig_der(x):
     return x * (1 - x)
 
 def cost(y_hat,y):
     return (y_hat-y)**2
 
-def cost_der(i,y_hat,y):
-    return 2*i*(y_hat-y)
+def cost_der(y_hat,y):
+    return 2*(y_hat-y)
 
 def weighted_sum(bias, weights, inputs):
-    sum = 0
-    for input in range(inputs):
-        sum += input * weights[input]
-    return sum + bias
+    weightSum = 0
+    for i in range(len(inputs)):
+        for j in range(len(inputs[i])):
+            weightSum += inputs[i][j] * weights[i][j]
+    return weightSum + bias
         
+def out_err_grad(y_hat, y):
+     return cost_der(y_hat, y) * sig_der(y_hat)
 
 
 
