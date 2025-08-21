577345c3a3730bb5eda2dbfa41057d63a836d6c4
ryandeel <164122262+ryandeel@users.noreply.github.com>
2025-08-21 18:58:56 -0500

    reworked a litttttle bit of the mlp and keeping the test file for any library functions we want to test

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -16,15 +16,22 @@ X = [(0,0), (0,1), (1,0), (1,1)]
 class MultiLayerPerceptron:
     def __init__(self):
         # thanks python libraries for random
-        self.weights = np.random.rand(2)
-        self.bias = random.uniform(0,1)
+        #self.weights = np.random.rand(2) # commenting this, as it works for a singular layer if we want test it works but not for multiple
+
+        # self.weights_hidden add these
+        # self.weights_output add these
+
+        # self.bias = 1
+        self.bias_hidden = np.ones((1, 2)) # size of hidden layer
+        self.bias_output = 1 # size of output layer
+        print(f"Initialized weights: {self.weights}, bias: {self.bias}")
 
     def step_function(self, x):
         return 1 if x >= 0 else 0
     
-    def predict(self, x):
+    def predict(self, X):
         # calculate weighted sum multiplying weight vetor with input vector
-        weighted_sum = np.dot(self.weights, x) + self.bias
+        weighted_sum = np.dot(X, self.weights) + self.bias
         return self.step_function(weighted_sum)
     
     def train(self, X, y1):
@@ -37,9 +44,13 @@ class MultiLayerPerceptron:
                 y_pred = self.predict(xi)
                 error = target - y_pred
                 if error != 0:
-                    self.weights += error * prop.LEARNING_RATE * xi
+                    # stack overflow said i gotta turn it into np aray to multiply float by a sequence 
+                    self.weights += error * prop.LEARNING_RATE * np.asarray(xi)
                     self.bias += error * prop.LEARNING_RATE
                     totalErrors += 1
+
+            # chatgpt print statement for testing
+            print(f"Epoch {epochs}, Weights: {self.weights}, Bias: {self.bias}, Errors: {totalErrors}")
 
             epochs += 1
             print("total errors: ", totalErrors)
@@ -51,3 +62,5 @@ class MultiLayerPerceptron:
 y1 = [0, 1, 1, 1]
 # targets for below
 y2 = [1, 1, 1, 0]
+# targets for xor problem
+xor = [0, 1, 1, 0]
---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+weights_input_hidden = np.random.randn(2, 2)
+weights_hidden_output = np.random.randn(2, 1)
+
+print(np.ones((1, 2)))
+print(np.ones((1,1)))
+
+print(np.random.rand(2))
+print(np.random.rand(2, 2))
+print(np.random.randn(2, 2))
